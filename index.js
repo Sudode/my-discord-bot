fd8e69f37fc9fc2e852e1d0fcb9279d953a8cbbb
Liam Halls <77005353+Sudode@users.noreply.github.com>
2021-03-17 19:13:44 +0000

    cleaner code

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const token = config.token;
 
 client.login(token)
 
-client.once('ready', ()=>{
+client.once('ready', () => {
     console.log("Hello World!")
 })
 
-client.on('message', (message)=>{
+client.on('message', (message) => {
     if (message.content.startsWith(prefix)) {
         const args = message.content.slice(prefix.length).split(/ +/)
         const command = args.shift().toLowerCase()
         // moderation cmds
         if (command === "ban") {
-            if(!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("No perms")
+            if (!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("No perms")
             let member = message.mentions.members.first();
-            if(member){
-                try{
+            if (member) {
+                try {
                     member.ban()
                     message.channel.send(`${member} was banned!`)
-                }catch(err) {
+                } catch (err) {
                     console.log(err)
                 }
-            }else{
+            } else {
                 message.channel.send("No member")
             }
             return;
-        }else if(command === "kick"){
+        } else if (command === "kick") {
             if (!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send("No perms")
             let member = message.mentions.members.first();
-            if(member){
-                try{
+            if (member) {
+                try {
                     member.kick()
                     message.channel.send(`${member} was kicked!`)
-                }catch(err) {
+                } catch (err) {
                     console.log(err)
                 }
-            }else{
+            } else {
                 message.channel.send("No member")
             }
             return;
-        }else if (command === "mute") {
+        } else if (command === "mute") {
             if (!message.member.permissions.has("MANAGE_ROLES")) return message.channel.send("No perms")
             member = message.mentions.members.first();
             const role = message.guild.roles.cache.get('id');
@@ -55,19 +55,19 @@ client.on('message', (message)=>{
                     } catch (err) {
                         console.log(err)
                     }
-                }else{
+                } else {
                     message.channel.send("No role")
                 }
-            }else{
+            } else {
                 message.channel.send("No member")
             }
             return;
         }
 
         // fun cmds
-        else if(command === "meme"){
+        else if (command === "meme") {
             const randomNumber = Math.floor(Math.random() * 50);
-            message.channel.send({files: [`./images/${randomNumber}`]})
+            message.channel.send({ files: [`./images/${randomNumber}`] })
             return;
         }
     }
